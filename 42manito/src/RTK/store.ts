0f8100c7e0645664388cc29bdaf370215c0e129f
9utty <leegh4250@gmail.com>
2023-07-03 18:49:29 +0900

    생성: redux store 생성

---
+++ 42manito/src/RTK/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { userApi } from "./Api/User";
+
+export const store = configureStore({
+  reducer: {
+    [userApi.reducerPath]: userApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(userApi.middleware),
+});
+
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
