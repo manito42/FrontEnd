42bb586621f45f8dab46e01cbdd6666fe190d87e
9utty <leegh4250@gmail.com>
2023-07-03 18:49:16 +0900

    수정: RTK 설정으로 인한 주입 코드 입력

--- 42manito/src/pages/_app.tsx
+++ 42manito/src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { Provider } from "react-redux";
+import store from "@/RTK/store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   );
 }
