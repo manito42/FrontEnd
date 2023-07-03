6fc6fcfefac886c8fe8bc66a1a09950f6a9dff77
9utty <leegh4250@gmail.com>
2023-07-03 18:20:45 +0900

    수정: 홈 레이아웃 적용

--- 42manito/src/pages/index.tsx
+++ 42manito/src/pages/index.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <Head>
-      <title>42Mainto</title>
-      <meta name="description" content="오늘도 빡코딩!" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <Layout>
+      {" "}
+      <Head>
+        <title>42Mainto</title>
+        <meta name="description" content="오늘도 빡코딩!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </Layout>
   );
 }
