fa0d90349b7c03ce8324ccf0982d6a877accf9f5
9utty <leegh4250@gmail.com>
2023-07-03 21:17:55 +0900

    기능추가: 앱 전반적으로 사용할 head 태그 위치 변경

--- 42manito/src/pages/_app.tsx
+++ 42manito/src/pages/_app.tsx
@@ -3,11 +3,22 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { Provider } from "react-redux";
 import store from "@/RTK/store";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <ThemeProvider attribute="class">
+        <Head>
+          <title>42Mainto</title>
+          <meta name="description" content="오늘도 빡코딩!" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="https://user-images.githubusercontent.com/86397600/250159799-efa4ccf2-daeb-46f0-8679-655e4f2587df.png"
+          />
+        </Head>
         <Component {...pageProps} />
       </ThemeProvider>
     </Provider>
