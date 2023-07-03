965d28458410f45f24d081ecc5838d9cf7e8f7bb
9utty <leegh4250@gmail.com>
2023-07-03 18:30:41 +0900

    수정: Next 버전 문제로 인한 Link태그 안 a 태그를 사용 못해서 a태그삭제

--- 42manito/src/components/Footer.tsx
+++ 42manito/src/components/Footer.tsx
@@ -6,13 +6,11 @@ const Footer: React.FC = () => {
       <footer className="body-font">
         <div className="">
           <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-            <Link href="/">
-              <a
-                id="link"
-                className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
-              >
-                <span className="ml-3 text-xl">42Manito</span>
-              </a>
+            <Link
+              href="/"
+              className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
+            >
+              <span className="ml-3 text-xl">42Manito</span>
             </Link>
             <span className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
               42Manito ©2023
--- 42manito/src/components/Header.tsx
+++ 42manito/src/components/Header.tsx
@@ -7,42 +7,39 @@ const Header: React.FC = () => {
     <>
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <Link href="/">
-            <a
-              id="link"
-              className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+          <Link
+            href="/"
+            className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+              viewBox="0 0 24 24"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-              </svg>
-              <span className="ml-3 text-xl">42Manito</span>
-            </a>
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span className="ml-3 text-xl">42Manito</span>
           </Link>
 
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link href="/">
-              <a id="link" className="mr-5 hover:text-gray-900">
-                홈
-              </a>
+            <Link href="/" className="mr-5 hover:text-gray-900">
+              홈
             </Link>
 
-            <Link href="/Hashtag">
-              <a id="link" className="mr-5 hover:text-gray-900">
-                해시태그
-              </a>
+            <Link href="/Hashtag" className="mr-5 hover:text-gray-900">
+              해시태그
             </Link>
 
-            <Link href="/Profile">
-              <a id="link" className="mr-5 hover:text-gray-900" />
+            <Link href="/Profile" className="mr-5 hover:text-gray-900">
+              프로필
+            </Link>
+            <Link href="/Profile" className="mr-5 hover:text-gray-900">
+              메뉴
             </Link>
           </nav>
           {/* 다크모드 토글 버튼 작업해야함 */}
