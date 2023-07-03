e2dfcf0af5a0e1fb4c3141be6de7bd7db9ddb84e
9utty <leegh4250@gmail.com>
2023-07-03 18:20:21 +0900

    생성: 상단 고정 헤더 생성

--- 42manito/src/components/Header.tsx
+++ 42manito/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import DarkModeToggleButton from "./DarkModeButton";
 
 const Header: React.FC = () => {
   return (
@@ -7,7 +8,10 @@ const Header: React.FC = () => {
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link href="/">
-            <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <a
+              id="link"
+              className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -26,15 +30,19 @@ const Header: React.FC = () => {
 
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
             <Link href="/">
-              <a className="mr-5 hover:text-gray-900">홈</a>
+              <a id="link" className="mr-5 hover:text-gray-900">
+                홈
+              </a>
             </Link>
 
             <Link href="/Hashtag">
-              <a className="mr-5 hover:text-gray-900">해시태그</a>
+              <a id="link" className="mr-5 hover:text-gray-900">
+                해시태그
+              </a>
             </Link>
 
             <Link href="/Profile">
-              <a href="" className="mr-5 hover:text-gray-900" />
+              <a id="link" className="mr-5 hover:text-gray-900" />
             </Link>
           </nav>
           {/* 다크모드 토글 버튼 작업해야함 */}
