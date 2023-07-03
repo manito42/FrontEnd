1c9647ff12ab411c905502b66037f47db51a98d8
9utty <leegh4250@gmail.com>
2023-07-03 20:59:40 +0900

    컴포넌트: Search 컴포넌트 작성

---
+++ 42manito/src/pages/Search.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+const Search: React.FC = () => {
+  return (
+    <>
+      <Layout>
+        <div className="app-container">검색창</div>
+      </Layout>
+    </>
+  );
+};
+
+export default Search;
