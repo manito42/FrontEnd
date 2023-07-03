3b7d74f8542cae3cca2497b0edb7cdc73b78bfca
9utty <leegh4250@gmail.com>
2023-07-03 18:03:06 +0900

    feat: 새로운 프로젝트 생성

---
+++ 42manito/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  domains: ["images.unsplash.com"],
+};
+
+module.exports = nextConfig;
---
+++ 42manito/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <Head>
+      <title>42Mainto</title>
+      <meta name="description" content="오늘도 빡코딩!" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
