38c9ac4f54aee7d3b6fb242e1c40a5f6df7d8249
9utty <leegh4250@gmail.com>
2023-07-03 20:54:26 +0900

    기능수정: 상단 아이콘 입력

--- 42manito/src/pages/index.tsx
+++ 42manito/src/pages/index.tsx
@@ -9,7 +9,12 @@ export default function Home() {
       <Head>
         <title>42Mainto</title>
         <meta name="description" content="오늘도 빡코딩!" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="https://user-images.githubusercontent.com/86397600/250159799-efa4ccf2-daeb-46f0-8679-655e4f2587df.png"
+        />
       </Head>
     </Layout>
   );
