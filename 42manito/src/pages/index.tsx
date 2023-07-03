84ff3d21cc5e4029be265244266c416d11b787a2
9utty <leegh4250@gmail.com>
2023-07-03 21:18:23 +0900

    기능수정: Head 태그 위치변경으로 인한 Home 컴포넌트 수정

--- 42manito/src/pages/index.tsx
+++ 42manito/src/pages/index.tsx
@@ -6,16 +6,7 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout>
-      <Head>
-        <title>42Mainto</title>
-        <meta name="description" content="오늘도 빡코딩!" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="https://user-images.githubusercontent.com/86397600/250159799-efa4ccf2-daeb-46f0-8679-655e4f2587df.png"
-        />
-      </Head>
+      <div></div>
     </Layout>
   );
 }
