b4a53b3ccdf7406150ef20c0aa56430a2e87aea4
9utty <leegh4250@gmail.com>
2023-07-03 18:20:32 +0900

    생성: 앱 레이아웃 생성

--- 42manito/src/components/Layout.tsx
+++ 42manito/src/components/Layout.tsx
@@ -1,1 +1,19 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div className="bg-primary">
+      <Header />
+      <div>{children}</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
