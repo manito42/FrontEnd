73bf07a70f7544ef9741e3a50b3b6bb5d2f33890
9utty <leegh4250@gmail.com>
2023-07-04 19:36:03 +0900

    생성: 엔롤 컴포넌트 생성

---
+++ 42manito/src/components/home/Enroll.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Enroll = () => {
+  return (
+    <div className="">
+      <div>멘토상황</div>
+      <div>멘티상황</div>
+    </div>
+  );
+};
+
+export default Enroll;
