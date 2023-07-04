5fee885a5cc1b200d24ec55ae1b9f79e6a17e6fe
9utty <leegh4250@gmail.com>
2023-07-04 19:36:29 +0900

    기능추가: 홈에 엔롤컴포넌트 추가

--- 42manito/src/pages/index.tsx
+++ 42manito/src/pages/index.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import Layout from "../components/Layout";
+import Enroll from "@/components/home/Enroll";
 
 export default function Home() {
   return (
     <Layout>
-      <div></div>
+      <Enroll />
     </Layout>
   );
 }
