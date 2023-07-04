5e8dfd8971fff0b471e5773fe23436d33726202c
9utty <leegh4250@gmail.com>
2023-07-04 19:36:16 +0900

    생성: 멘토카드리스트 생성

---
+++ 42manito/src/components/home/MentorCardList.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MentorCard from "../MentorCard";
+
+const MentorCardList = () => {
+  return (
+    <div>
+      <MentorCard data={} />
+    </div>
+  );
+};
+
+export default MentorCardList;
+
+const mocCard: MentorProfileDto[] = [{}];
