abb03fb1cf3dbe933d169807563c66bf8993a777
9utty <leegh4250@gmail.com>
2023-07-05 19:12:32 +0900

    기능추가: MuiRate 코드 추가

--- 42manito/src/components/mentor/MentorModal.tsx
+++ 42manito/src/components/mentor/MentorModal.tsx
@@ -1,6 +1,7 @@
 import { MentorProfileDto } from "@/Types/MentorProfileDto";
 import Image from "next/image";
 import React from "react";
+import MuiRate from "../MuiRate";
 
 interface props {
   onClose: () => void;
@@ -77,35 +78,32 @@ const MentroModal = ({ isVisible, onClose, data }: props) => {
               </div>
               <div className="text-center mt-28">
                 <h3 className="text-4xl font-semibold leading-normal text-blueGray-700">
-                  Jenna Stones
+                  {data.nickname}
                 </h3>
-                <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
-                  <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
-                  Los Angeles, California
-                </div>
-                <div className="mb-2 text-blueGray-600 mt-10">
-                  <i className="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>
-                  Solution Manager - Creative Tim Officer
-                </div>
-                <div className="mb-2 text-blueGray-600">
-                  <i className="fas fa-university mr-2 text-lg text-blueGray-400"></i>
-                  University of Computer Science
-                </div>
               </div>
               <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
                 <div className="flex flex-wrap justify-center">
                   <div className="w-full lg:w-9/12 px-4">
                     <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
-                      An artist of considerable range, Jenna the name taken by
-                      Melbourne-raised, Brooklyn-based Nick Murphy writes,
-                      performs and records all of his own music, giving it a
-                      warm, intimate feel with a solid groove structure. An
-                      artist of considerable range.
+                      {data.description}
                     </p>
-                    <a href="#pablo" className="font-normal text-pink-500">
-                      Show more
-                    </a>
                   </div>
+                </div>
+                {/* <div className="w-full lg:w-full px-4">
+                  <div>멘토링 평점</div>
+                  <MuiRate IsReadOnly={true} Value={5.0} />
+                  <div>멘티 평점</div>
+                  <MuiRate IsReadOnly={true} Value={5.0} />
+                </div> */}
+                <div className="flex items-start mt-2 m-auto">
+                  {data.hashtags.map((aTag) => (
+                    <h1
+                      className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30"
+                      key={aTag.id}
+                    >
+                      {aTag.name}
+                    </h1>
+                  ))}
                 </div>
               </div>
             </div>
