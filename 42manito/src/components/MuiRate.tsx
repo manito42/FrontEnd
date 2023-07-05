6d58e6e6991844799d0bc2f5b0932139cc6f61d3
9utty <leegh4250@gmail.com>
2023-07-05 19:12:19 +0900

    기능추가: Rate 추가 추후에 업데이트 예정

---
+++ 42manito/src/components/MuiRate.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Rating, { IconContainerProps } from "@mui/material/Rating";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
+import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied";
+import SentimentSatisfiedIcon from "@mui/icons-material/SentimentSatisfied";
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAltOutlined";
+import SentimentVerySatisfiedIcon from "@mui/icons-material/SentimentVerySatisfied";
+
+const StyledRating = styled(Rating)(({ theme }) => ({
+  "& .MuiRating-iconEmpty .MuiSvgIcon-root": {
+    color: theme.palette.action.disabled,
+  },
+}));
+
+const customIcons: {
+  [index: string]: {
+    icon: React.ReactElement;
+    label: string;
+  };
+} = {
+  1: {
+    icon: <SentimentVeryDissatisfiedIcon color="error" />,
+    label: "Very Dissatisfied",
+  },
+  2: {
+    icon: <SentimentDissatisfiedIcon color="error" />,
+    label: "Dissatisfied",
+  },
+  3: {
+    icon: <SentimentSatisfiedIcon color="warning" />,
+    label: "Neutral",
+  },
+  4: {
+    icon: <SentimentSatisfiedAltIcon color="success" />,
+    label: "Satisfied",
+  },
+  5: {
+    icon: <SentimentVerySatisfiedIcon color="success" />,
+    label: "Very Satisfied",
+  },
+};
+
+function IconContainer(props: IconContainerProps) {
+  const { value, ...other } = props;
+  return <span {...other}>{customIcons[value].icon}</span>;
+}
+
+interface MuiRateProps {
+  Value: number;
+  IsReadOnly: boolean;
+}
+
+const MuiRate = (props: MuiRateProps) => {
+  let value;
+  if (props.Value < 1) {
+    value = 1;
+  } else {
+    value = props.Value;
+  }
+  return (
+    <>
+      <StyledRating
+        name="highlight-selected-only"
+        defaultValue={Math.floor(value)}
+        IconContainerComponent={IconContainer}
+        getLabelText={(value: number) => customIcons[value].label}
+        highlightSelectedOnly
+        readOnly={props.IsReadOnly}
+      />
+    </>
+  );
+};
+
+export default MuiRate;
