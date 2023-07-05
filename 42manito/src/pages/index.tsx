740d642b497d1bcd30e71dc99e26348ff99722d1
9utty <leegh4250@gmail.com>
2023-07-05 15:01:23 +0900

    기능추가: 홈 컴포넌트 작성

--- 42manito/src/pages/index.tsx
+++ 42manito/src/pages/index.tsx
@@ -3,11 +3,61 @@ import { Inter } from "next/font/google";
 import Head from "next/head";
 import Layout from "../components/Layout";
 import Enroll from "@/components/home/Enroll";
+import { useState } from "react";
+import { MentorProfileDto } from "@/Types/MentorProfileDto";
+import { mockMentorProfiles } from "../../mocData/mentorData";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Row, Spin } from "antd";
+import MentorCard from "@/components/MentorCard";
 
 export default function Home() {
+  const [mentor, setMentor] = useState<MentorProfileDto[]>(
+    mockMentorProfiles.slice(0, 12)
+  );
+  const [page, setPage] = useState(1);
+
+  const fetchMoreData = () => {
+    setPage(page + 1);
+    if (mentor.length >= mockMentorProfiles.length) {
+      return; // 모든 데이터를 불러왔으면 추가로 더 불러오지 않음
+    }
+    const nextCards = mockMentorProfiles.slice(
+      mentor.length,
+      mentor.length + 12
+    );
+    setMentor([...mentor, ...nextCards]);
+  };
+
   return (
     <Layout>
-      <Enroll />
+      <div className="app-container">
+        <InfiniteScroll
+          dataLength={mentor.length}
+          next={fetchMoreData}
+          hasMore={mentor.length < mockMentorProfiles.length}
+          loader={
+            <div
+              className="example"
+              style={{ verticalAlign: "middle", alignContent: "center" }}
+            >
+              <Spin />
+            </div>
+          }
+        >
+          <Enroll />
+          <Row style={{ display: "flex", flexWrap: "wrap" }}>
+            {mentor.map((mentor) => (
+              <div
+                className="px-3 mb-10  sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4"
+                key={mentor.id}
+                style={{ display: "flex" }}
+              >
+                <MentorCard data={mentor} key={mentor.id} />
+              </div>
+            ))}
+          </Row>
+        </InfiniteScroll>
+      </div>
     </Layout>
   );
 }
