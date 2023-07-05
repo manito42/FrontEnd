aba613b506bb1f253a426c18d9f1e3980472b4a5
9utty <leegh4250@gmail.com>
2023-07-05 22:10:00 +0900

    기능추가: 페이지 구현중 Alert 추가

--- 42manito/src/pages/Ranking.tsx
+++ 42manito/src/pages/Ranking.tsx
@@ -1,9 +1,22 @@
 import Layout from "@/components/Layout";
-import React from "react";
+import InfoAlert from "@/components/alert/InfoAlert";
+import React, { useState } from "react";
 
 const Ranking: React.FC = () => {
+  const [isAlert, setIsAlert] = useState(true);
+
+  const onCloseAlert = () => {
+    setIsAlert(false);
+    location.href = "/";
+  };
+
   return (
     <>
+      <InfoAlert
+        isAlert={isAlert}
+        close={onCloseAlert}
+        msg={"기능구현중입니다."}
+      />
       <Layout>
         <div className="app-container">랭킹</div>
       </Layout>
