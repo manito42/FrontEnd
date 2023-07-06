f3fb7990a696471211893d7162e8c88e336e5afe
9utty <leegh4250@gmail.com>
2023-07-06 22:11:16 +0900

    기능추가: Enroll 페이지 구성

---
+++ 42manito/src/components/enroll/Enroll.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { mocReservation } from "../../../mocData/mocReservation";
+import { ReservationGetDto } from "@/Types/Reservations";
+import EnrollCard from "./components/ErollCard";
+import InfiniteScroll from "react-infinite-scroll-component";
+import TypeIt from "typeit-react";
+
+const Enroll = () => {
+  const reservation: ReservationGetDto[] = mocReservation;
+
+  const mentor: ReservationGetDto[] = reservation.filter(
+    (value) =>
+      value.mentorId === 1 &&
+      (value.status === "ACCEPT" || value.status === "REQUEST")
+  );
+
+  const mentee: ReservationGetDto[] = reservation.filter(
+    (value) =>
+      value.menteeId === 1 &&
+      (value.status === "ACCEPT" || value.status === "REQUEST")
+  );
+
+  console.log(`mentor: ${mentor.length} mentee: ${mentee.length}`);
+
+  return (
+    <div className="my-16 w-full">
+      <div className="w-[100%] dark:bg-white/40 bg-black/40 h-[1px]" />
+      <div className="flex justify-center items-center my-16 2xl:flex-row flex-col">
+        <div className="flex text-4xl">
+          <TypeIt
+            options={{
+              loop: false,
+              strings: ["Mentor."],
+              speed: 40,
+              waitUntilVisible: true,
+            }}
+          />
+        </div>
+        <div className="overflow-y-scroll w-full mx-20 h-80">
+          {mentor.map((data) => (
+            <EnrollCard data={data} key={data.id} isMentor={true} />
+          ))}
+        </div>
+        <div className="text-4xl">
+          <TypeIt
+            options={{
+              loop: false,
+              strings: ["Mentee."],
+              speed: 40,
+              waitUntilVisible: true,
+            }}
+          />
+        </div>
+        <div className="overflow-y-scroll h-80 w-full mx-20">
+          {mentee.map((data) => (
+            <EnrollCard data={data} key={data.id} isMentor={false} />
+          ))}
+        </div>
+      </div>
+      <div className="w-[100%] dark:bg-white/40 bg-black/40 h-[1px]" />
+    </div>
+  );
+};
+
+export default Enroll;
