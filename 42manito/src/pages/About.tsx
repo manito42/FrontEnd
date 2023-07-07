d5d2639da2b182fad2fd9e485ea2dd2c62dbe09c
9utty <leegh4250@gmail.com>
2023-07-07 18:49:32 +0900

    생성: About 페이지 생성

---
+++ 42manito/src/pages/About.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/layout/Layout";
+import React from "react";
+
+const About = () => {
+  return (
+    <Layout>
+      <div>어바웃페이지</div>
+    </Layout>
+  );
+};
+
+export default About;
