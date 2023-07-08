84007ee7a9cd1b299139b038162ce9f16cbe6069
9utty <leegh4250@gmail.com>
2023-07-08 18:01:54 +0900

    스타일: mentorCard 스타일 수정

--- 42manito/src/components/mentor/MentorCard.tsx
+++ 42manito/src/components/mentor/MentorCard.tsx
@@ -10,22 +10,28 @@ interface props {
 const MentorCard = ({ data, onOpen }: props) => {
   return (
     <>
-      <div className="mentor-card" onClick={() => onOpen(data)}>
+      <div
+        className="mentor-card"
+        onClick={() => onOpen(data)}
+        style={{ width: "230px", maxHeight: "500px", overflow: "hidden" }}
+      >
         <Image
-          className="rounded-t-xl object-cover layout-responsive flex "
+          className="rounded-t-xl object-cover layout-responsive flex w-full"
           src={data.user.profileImage}
           alt="cover image"
-          width={500}
-          height={500}
+          width={300}
+          height={300}
           quality={100}
         />
-        <div className="p-4 flex flex-col">
-          <h2 className="text-xl font-bold">{data.user.nickname}</h2>
-          <h3 className="mt-4 text-l">{data.shortDescription}</h3>
-          <div className="flex items-start mt-2 m-auto">
+        <div className="placeholder:p-4 flex flex-col justify-center items-center ml-3 mr-3 mb-4">
+          <h2 className="text-xl font-bold mt-5">{data.user.nickname}</h2>
+          <h3 className=" text-l p-4 overflow-y-scroll h-[100px]">
+            {data.shortDescription}
+          </h3>
+          <div className="flex flex-row mt-2 m-auto overflow-x-scroll w-full">
             {data.hashtags.map((aTag) => (
               <h6
-                className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30"
+                className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 "
                 key={aTag.id}
               >
                 {aTag.name}
