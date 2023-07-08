19dd29f8d1010c828d88297406fab7c8167e88fb
9utty <leegh4250@gmail.com>
2023-07-09 02:44:42 +0900

    기능추가: 랭킹페이지 구조 완료

--- 42manito/src/pages/Ranking.tsx
+++ 42manito/src/pages/Ranking.tsx
@@ -1,24 +1,84 @@
 import Layout from "@/components/layout/Layout";
 import InfoAlert from "@/components/alert/InfoAlert";
 import React, { useState } from "react";
+import { Divider } from "antd";
+import { mentorResDto } from "@/Types/MentorProfileDto";
+import { mockMentorProfiles } from "../../mocData/mentorData";
+import MentorCard from "@/components/mentor/MentorCard";
+import MentorModal from "@/components/mentor/MentorModal";
 
 const Ranking: React.FC = () => {
   const [isAlert, setIsAlert] = useState(true);
+  const mentor: mentorResDto[] = mockMentorProfiles;
+  const [isVisible, setIsVisible] = useState(false);
+  const [currMentor, setCurrMentor] = useState<mentorResDto>(
+    {} as mentorResDto
+  );
 
   const onCloseAlert = () => {
     setIsAlert(false);
     location.href = "/";
   };
 
+  const onOpen = (data: mentorResDto) => {
+    setIsVisible(true);
+    setCurrMentor(data);
+  };
+
+  const onClose = () => {
+    setIsVisible(false);
+  };
+
   return (
     <>
-      <InfoAlert
-        isAlert={isAlert}
-        close={onCloseAlert}
-        msg={"기능구현중입니다."}
-      />
       <Layout>
-        <div className="app-container">랭킹</div>
+        <div className="app-container">
+          <div
+            id="Total."
+            className="w-full flex flex-col justify-center items-center"
+          >
+            <span className="text-5xl font-bold">Total.</span>
+            <div className="flex flex-col justify-center items-center md:m-10">
+              <div>
+                <MentorCard data={mentor[3]} onOpen={onOpen} />
+              </div>
+              <div className="flex flex-row">
+                <div className="md:m-10">
+                  <MentorCard data={mentor[29]} onOpen={onOpen} />
+                </div>
+                <div className="md:m-10">
+                  <MentorCard data={mentor[25]} onOpen={onOpen} />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Divider className="bg-slate-700 dark:bg-slate-200" />
+          <div
+            id="Month."
+            className="w-full flex flex-col justify-center items-center"
+          >
+            <span className="text-5xl font-bold">Month.</span>
+            <div className="flex flex-col justify-center items-center md:m-10">
+              <div>
+                <MentorCard data={mentor[25]} onOpen={onOpen} />
+              </div>
+              <div className="flex flex-row">
+                <div className="md:m-10">
+                  <MentorCard data={mentor[18]} onOpen={onOpen} />
+                </div>
+                <div className="md:m-10">
+                  <MentorCard data={mentor[9]} onOpen={onOpen} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <MentorModal
+          isVisible={isVisible}
+          onClose={onClose}
+          data={currMentor}
+        />
       </Layout>
     </>
   );
