0eb106f901f1958d8276a6cffb00805ee41ce0bb
9utty <leegh4250@gmail.com>
2023-07-09 02:43:05 +0900

    생성: ProfileUpdate 컴포넌트 생성

---
+++ 42manito/src/components/Profile/ProfileUpdate.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout/Layout";
+import React from "react";
+
+const ProfileUpdate: React.FC = () => {
+  return (
+    <>
+      <Layout>
+        <div className="app-container">프로필업데이트</div>
+      </Layout>
+    </>
+  );
+};
+
+export default ProfileUpdate;
