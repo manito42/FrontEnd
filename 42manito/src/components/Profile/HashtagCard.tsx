d0ec33c268d3ea0af22ea1ac0e75ab72496b4fdb
9utty <leegh4250@gmail.com>
2023-07-09 18:41:23 +0900

    생성: HashtagCard 생성

---
+++ 42manito/src/components/Profile/HashtagCard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface props {
+  hashtag: string;
+}
+
+const HashtagCard = ({ hashtag }: props) => {
+  return (
+    <div className="flex flex-row mx-3">
+      <h6 className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 ">
+        {hashtag}
+      </h6>
+      <button className="text-slate-600 dark:text-white text-2xl font-bold">
+        X
+      </button>
+    </div>
+  );
+};
+
+export default HashtagCard;
