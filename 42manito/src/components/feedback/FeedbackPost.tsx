7e5747cdca7a0be66ea2c9e97ee46c87ecafcb49
9utty <leegh4250@gmail.com>
2023-07-09 18:40:35 +0900

    생성: FeedbackPost 컴포넌트 생성

---
+++ 42manito/src/components/feedback/FeedbackPost.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout/Layout";
+import React from "react";
+
+const FeedbackPost = () => {
+  return (
+    <>
+      <Layout>
+        <div className="app-container">멘티피드백</div>
+      </Layout>
+    </>
+  );
+};
+
+export default FeedbackPost;
