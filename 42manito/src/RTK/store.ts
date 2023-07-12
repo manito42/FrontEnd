6255842eb276c2de990d443c2c7dbd1fb190ef6e
9utty <leegh4250@gmail.com>
2023-07-12 19:53:35 +0900

    기능추가: store의 각각의 RTKQuery API 추가와 globalSlice 추가

--- 42manito/src/RTK/store.ts
+++ 42manito/src/RTK/store.ts
@@ -1,13 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
-import { userApi } from "./Api/User";
+import { feedbackApi } from "./Apis/Feedback";
+import { hashtagApi } from "./Apis/Hashtag";
+import { mentorApi } from "./Apis/Mentor";
+import { reservationApi } from "./Apis/Reservation";
+import { userApi } from "./Apis/User";
+import { rootReducer } from "./rootReducer";
+import { GlobalSlice } from "./Slices/Global";
 
 export const store = configureStore({
   reducer: {
+    rootReducers: rootReducer,
     [userApi.reducerPath]: userApi.reducer,
+    [mentorApi.reducerPath]: mentorApi.reducer,
+    [hashtagApi.reducerPath]: hashtagApi.reducer,
+    [feedbackApi.reducerPath]: feedbackApi.reducer,
+    [reservationApi.reducerPath]: reservationApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApi.middleware),
+    getDefaultMiddleware()
+      .concat(userApi.middleware)
+      .concat(mentorApi.middleware)
+      .concat(hashtagApi.middleware)
+      .concat(feedbackApi.middleware)
+      .concat(reservationApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
