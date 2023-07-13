193a6f2bd7b513e26821383d7db05b0c1ab339b4
9utty <leegh4250@gmail.com>
2023-07-13 17:09:49 +0900

    기능수정: Slice로 구성해서 필요없어진 코드 제거

--- 42manito/src/components/conect/ConnectModal.tsx
+++ 42manito/src/components/conect/ConnectModal.tsx
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
 
 interface Props {
-  viewConnectModal: boolean;
   message: string;
   onClose: () => void;
 }
 
-const ConnectModal = ({ viewConnectModal, message, onClose }: Props) => {
+const ConnectModal = ({ message, onClose }: Props) => {
   const [focus, setFocus] = useState(false);
   const [disabled, setDisabled] = useState(false);
-
-  if (viewConnectModal === false) return null;
 
   const handleFocusOut = () => {
     setFocus(true);
@@ -20,15 +17,11 @@ const ConnectModal = ({ viewConnectModal, message, onClose }: Props) => {
     }, 200);
   };
 
-  const handleDisabled = () => {
-    setDisabled(true);
-    setDisabled(false);
-  };
-
   const handleConnect = () => {
-    handleDisabled();
+    setDisabled(true);
     // TODO: 커넥트요청하기
     // handleFocusOut();
+    setDisabled(false);
   };
 
   return (
@@ -56,6 +49,7 @@ const ConnectModal = ({ viewConnectModal, message, onClose }: Props) => {
                   className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                   type="button"
                   onClick={handleConnect}
+                  disabled={disabled}
                 >
                   Yes
                 </button>
