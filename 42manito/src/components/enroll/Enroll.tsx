f2e39334cdab695f4d773e7d7def3ebeff4d1493
9utty <leegh4250@gmail.com>
2023-07-14 18:21:16 +0900

    코드수정: 스타일 수정

--- 42manito/src/components/enroll/Enroll.tsx
+++ 42manito/src/components/enroll/Enroll.tsx
@@ -21,61 +21,64 @@ const Enroll = () => {
   );
 
   return (
-    <div className="my-16 w-full">
+    <div className="my-16 w-full h-[40vh]">
       <div className="w-[100%] dark:bg-white/40 bg-black/40 h-[1px]" />
-      <div className="flex justify-center items-center my-16 md:flex-row flex-col">
-        <div className="text-4xl">
-          <TypeIt
-            options={{
-              loop: false,
-              speed: 70,
-              waitUntilVisible: true,
-              cursor: false,
-            }}
-            getBeforeInit={(instance) => {
-              instance
-                .type("Mentee?")
-                .pause(500)
-                .delete(3)
-                .pause(500)
-                .type("or.");
+      <div className="grid grid-rows-2 lg:grid-cols-2 gap-y-6 lg:gap-y-0 items-center my-16 md:flex-row flex-col">
+        <div className="flex-row justify-center w-full lg:flex items-center">
+          <div className="text-4xl">
+            <TypeIt
+              options={{
+                loop: false,
+                speed: 70,
+                waitUntilVisible: true,
+                cursor: false,
+              }}
+              getBeforeInit={(instance) => {
+                instance
+                  .type("Mentee?")
+                  .pause(500)
+                  .delete(3)
+                  .pause(500)
+                  .type("or.");
 
-              return instance;
-            }}
-          />
+                return instance;
+              }}
+            />
+          </div>
+          <div className="overflow-y-auto w-full mx-10 h-[30vh]">
+            {mentor.map((data) => (
+              <EnrollCard data={data} key={data.id} isMentor={true} />
+            ))}
+          </div>
         </div>
-        <div className="overflow-y-auto w-full mx-10">
-          {mentor.map((data) => (
-            <EnrollCard data={data} key={data.id} isMentor={true} />
-          ))}
-        </div>
-        <div className="text-4xl">
-          <TypeIt
-            options={{
-              loop: false,
-              speed: 70,
-              waitUntilVisible: true,
-              cursor: false,
-            }}
-            getBeforeInit={(instance) => {
-              instance
-                .type(`Mentor?`)
-                .pause(500)
-                .delete(3)
-                .pause(500)
-                .type("ee.");
+        <div className="flex-row justify-center w-full lg:flex items-center ">
+          <div className="text-4xl">
+            <TypeIt
+              options={{
+                loop: false,
+                speed: 70,
+                waitUntilVisible: true,
+                cursor: false,
+              }}
+              getBeforeInit={(instance) => {
+                instance
+                  .type(`Mentor?`)
+                  .pause(500)
+                  .delete(3)
+                  .pause(500)
+                  .type("ee.");
 
-              return instance;
-            }}
-          />
-        </div>
-        <div className="overflow-y-auto w-full mx-20">
-          {mentee.map((data) => (
-            <EnrollCard data={data} key={data.id} isMentor={false} />
-          ))}
+                return instance;
+              }}
+            />
+          </div>
+          <div className="overflow-y-auto w-full mx-10 h-[30vh]">
+            {mentee.map((data) => (
+              <EnrollCard data={data} key={data.id} isMentor={false} />
+            ))}
+          </div>
         </div>
       </div>
-      <div className="w-[100%] dark:bg-white/40 bg-black/40 h-[1px]" />
     </div>
   );
 };
