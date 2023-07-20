91468fa68532804d9093584c28b7a6b38d5c0d9b
9utty <leegh4250@gmail.com>
2023-07-20 21:34:45 +0900

    코드수정: userApi 수정

--- 42manito/src/RTK/Apis/User.ts
+++ 42manito/src/RTK/Apis/User.ts
@@ -1,67 +1,14 @@
-import { UserResDto } from "@/Types/UserResDto";
-import { baseQueryWithGlobalErrorHandler } from "@/utils/BaseQueryErrorHandler";
-import customAxios from "@/utils/CustomAxios";
-import {
-  BaseQueryFn,
-  createApi,
-  fetchBaseQuery,
-  FetchBaseQueryError,
-} from "@reduxjs/toolkit/query/react";
-import { AxiosError, AxiosRequestConfig } from "axios";
-
-const userBaseQuery =
-  (
-    { baseUrl }: { baseUrl: string } = { baseUrl: "" }
-  ): BaseQueryFn<
-    {
-      url: string;
-      method: AxiosRequestConfig["method"];
-      data?: AxiosRequestConfig["data"];
-      params?: AxiosRequestConfig["params"];
-    },
-    unknown,
-    unknown
-  > =>
-  async ({ url, method, data, params }) => {
-    try {
-      const result = await customAxios({
-        url: baseUrl + url,
-        method,
-        data,
-        params,
-      });
-      return { data: result.data };
-    } catch (axiosError) {
-      let err = axiosError as AxiosError;
-      // 나중에 여기서 에러처리들을 하면 됨 (400, 404, 409 등)
-
-      return {
-        error: {
-          status: err.response?.status,
-          data: err.response?.data || err.message,
-        },
-      };
-    }
-  };
-
-// User RTKQuery 코드
+import { UserDefaultDto } from "@/Types/Users/UserDefault.dto";
+import { BaseQuery } from "@/utils/BaseQuery";
+import { createApi } from "@reduxjs/toolkit/dist/query/react";
 
 export const userApi = createApi({
   reducerPath: "userApi",
-  baseQuery: userBaseQuery({
+  baseQuery: BaseQuery({
     baseUrl: `${process.env.NEXT_PUBLIC_MOC_URL}`,
   }),
   endpoints: (builder) => ({
-    getAllUsers: builder.query<UserResDto[], { take: number; page: number }>({
-      query: ({ take, page }) => {
-        return {
-          url: `/users/`,
-          method: "GET",
-        };
-      },
-      keepUnusedDataFor: 100,
-    }),
-    getUser: builder.query<UserResDto, { id: number }>({
+    getUser: builder.query<UserDefaultDto, { id: number }>({
       query: ({ id }) => {
         return {
           url: `/users/${id}`,
@@ -73,4 +20,4 @@ export const userApi = createApi({
   }),
 });
 
-export const { useGetAllUsersQuery, useGetUserQuery } = userApi;
+export const { useGetUserQuery } = userApi;
