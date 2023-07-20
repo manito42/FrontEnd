758b6386275490d1bdcbcc13067fe5dd3038e62f
9utty <leegh4250@gmail.com>
2023-07-20 21:30:47 +0900

    코드수정: 테스트용 이미지 CDN 주소 설정

--- 42manito/next.config.js
+++ 42manito/next.config.js
@@ -3,7 +3,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com", "cdn.intra.42.fr", "i.pravatar.cc"],
+    domains: [
+      "images.unsplash.com",
+      "cdn.intra.42.fr",
+      "i.pravatar.cc",
+      "cloudflare-ipfs.com",
+    ],
   },
 };
 
