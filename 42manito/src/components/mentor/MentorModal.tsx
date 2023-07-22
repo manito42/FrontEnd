029782da3cc75cfb221f8b41ed84e037d6ad0e57
9utty <leegh4250@gmail.com>
2023-07-22 18:36:50 +0900

    코드수정: 로그인안하고 Owner의 카드일때 connect 버튼 비활성화

--- 42manito/src/components/mentor/MentorModal.tsx
+++ 42manito/src/components/mentor/MentorModal.tsx
@@ -10,6 +10,9 @@ const MentorModal = () => {
   const currMentorState = useSelector(
     (state: RootState) => state.rootReducers.currMentor
   );
+  const Owner = useSelector(
+    (state: RootState) => state.rootReducers.global.uId
+  );
   const handleClose = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.currentTarget.id === "wrapper") handleZoomOut();
   };
@@ -68,13 +71,16 @@ const MentorModal = () => {
                 <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center"></div>
                 <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                   <div className="py-6 px-3 mt-32 sm:mt-0">
-                    <button
-                      className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
-                      type="button"
-                      onClick={() => handleConnectOpen()}
-                    >
-                      Connect
-                    </button>
+                    {Owner !== 0 &&
+                      Owner !== currMentorState.currMentor.user.id && (
+                        <button
+                          className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                          type="button"
+                          onClick={() => handleConnectOpen()}
+                        >
+                          Connect
+                        </button>
+                      )}
                   </div>
                 </div>
                 <div className="w-full lg:w-4/12 px-4 lg:order-1" id="Count">
