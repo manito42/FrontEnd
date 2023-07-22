5a4868c5638e3a4a579ef0c5af9904c2654e069b
9utty <leegh4250@gmail.com>
2023-07-22 17:04:27 +0900

    페이지최적화: 최적화 일단 완료 나중에 빌드 후 확인하고 추후에 도전해야할듯...

--- 42manito/src/pages/About.tsx
+++ 42manito/src/pages/About.tsx
@@ -1,12 +1,12 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable @next/next/no-img-element */
-// About.tsx
 import React, { useEffect, useRef, useState } from "react";
 import Layout from "@/components/layout/Layout";
-import TypeIt from "typeit-react";
-import AnimatedText from "@/components/typo/AnimationTypo";
-import TypoAction from "typo-action";
-import TypoActionTest from "@/components/about/typoAction";
+import dynamic from "next/dynamic";
+
+const About1 = dynamic(() => import("@/components/about/About1"));
+const About2 = dynamic(() => import("@/components/about/About2"));
+const About3 = dynamic(() => import("@/components/about/About3"));
+const About4 = dynamic(() => import("@/components/about/About4"));
+const About5 = dynamic(() => import("@/components/about/About5"));
 
 const About = () => {
   const observer = useRef<IntersectionObserver | null>(null);
@@ -34,8 +34,6 @@ const About = () => {
       const scrollInSection = scrollY - sectionTop;
       const percentOfSectionHeightScrolled =
         (scrollInSection / sectionHeight) * 100;
-
-      console.log(Math.min(percentOfSectionHeightScrolled, 100));
       htmlElement.style.setProperty(
         "--scroll",
         String(Math.min(percentOfSectionHeightScrolled, 100))
@@ -128,63 +126,11 @@ const About = () => {
 
   return (
     <Layout>
-      {/* Other elements */}
-      <div className="about" id="about1">
-        <div className="about-show" data-id="about1">
-          <div className="about1-content">
-            <div className="about-left text-white">
-              <TypoAction
-                text="No man lives alone."
-                className="typo-action1"
-                cursorView={false}
-                speed={80}
-              />
-            </div>
-            <div className="about-right">
-              <TypoAction
-                text="42Manito"
-                className="typo-action2"
-                speed={80}
-                delay={80 * 3}
-                cursorColor="blue"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="about" id="about2">
-        <div className="absolute w-[100vw] h-[100vh] opacity-70 bg-gray-700" />
-        <div className="about-show" data-id="about2">
-          <span className="about-title">
-            {`"42마니또에서 나도 가르치는 베테랑, 배우는 초심자가 될 수 있다!
-            42서울의 각양각색 멘토링 경험을 만끽해보세요."`}
-          </span>
-        </div>
-      </div>
-      <div className="about" id="about3">
-        <div className="absolute w-[100vw] h-[100vh] opacity-40 bg-black" />
-        <div className="about-show" data-id="about3">
-          <span className="about-title">{`"42마니또 통해 서로 지식과 취미를 나누며 성장하는 42서울의 정체성을 경험해보세요. 서로의 시너지를 느낄 수 있는 환경에서 더 큰 발전을 이루어 가요."`}</span>
-          <img src="pexels-fauxels-3183165.jpg" className="about-image" />
-        </div>
-      </div>
-      <div className="about" id="about4">
-        <div className="absolute w-[100vw] h-[100vh] opacity-40 bg-black" />
-        <div className="about-show" data-id="about4">
-          <img
-            src="pexels-ricardo-oliveira-13952574.jpg"
-            className="about-image"
-          />
-          <span className="about-title">
-            {`"멘토 신청으로 지식과 열정을 나누세요. 누구나 멘토가 될 수 있습니다!"`}
-          </span>
-        </div>
-      </div>
-      <div className="about" id="about5">
-        <div className="about-show" data-id="about5">
-          <span className="about-title">{`"42마니또와 함께 공부와 취미를 도전하며 도움과 격려를 받아보세요!"`}</span>
-        </div>
-      </div>
+      <About1 />
+      <About2 />
+      <About3 />
+      <About4 />
+      <About5 />
     </Layout>
   );
 };
