6ba98ca6db59f2d21e468ca5d16bd9dcac1cd884
9utty <leegh4250@gmail.com>
2023-07-23 01:31:37 +0900

    코드수정: 무한스크롤관련 오류 수정 및 data핸들링관련된 코드 수정

--- 42manito/src/pages/index.tsx
+++ 42manito/src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Layout from "../components/layout/Layout";
 import { useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { Divider } from "antd";
+import { Divider, Spin } from "antd";
 import { useSelector } from "react-redux";
-import { RootState } from "@/RTK/store";
-import { useGetHomeQuery } from "@/RTK/Apis/Home";
+import { RootState, useAppDispatch } from "@/RTK/store";
+import { useGetMentorsMutation } from "@/RTK/Apis/Home";
 import dynamic from "next/dynamic";
+import { initAllMentor, setAllMentor } from "@/RTK/Slices/Home";
 
 const Typo = dynamic(() => import("@/components/home/Typo"));
 const MentorCard = dynamic(() => import("@/components/mentor/MentorCard"));
@@ -15,32 +16,22 @@ export default function Home() {
   const currMentorState = useSelector(
     (state: RootState) => state.rootReducers.currMentor
   );
+  const allMentor = useSelector(
+    (state: RootState) => state.rootReducers.home.allMentor
+  );
+  const dispatch = useAppDispatch();
   const [page, setPage] = useState(0);
   const [hasMore, setHasMore] = useState(true);
 
-  const {
-    data: mentorCardData,
-    isError: mentorCardError,
-    isLoading: mentorCardLoading,
-    refetch,
-  } = useGetHomeQuery({ take: 12, page: page });
+  const [getMentors, { data, isLoading, error }] = useGetMentorsMutation();
 
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   const fetchMoreData = () => {
-    if (mentorCardData) {
-      if (mentorCardData.length % 12 !== 0) {
-        setHasMore(false);
-        return;
-      }
-      setPage(page + 1);
-      if (mentorCardError) {
-        alert("데이터를 불러오는데 실패했습니다.");
-        return;
-      }
-    }
+    getMentors({ take: 12, page: page });
+    setPage(page + 1);
   };
 
   useEffect(() => {
@@ -51,28 +42,37 @@ export default function Home() {
     }
   }, [currMentorState.openMentorModal]);
 
+  useEffect(() => {
+    if (data && !error) {
+      dispatch(setAllMentor(data));
+      if (data.length % 12 !== 0 || data.length === 0) {
+        setHasMore(false);
+      }
+    }
+  }, [isLoading]);
+
+  useEffect(() => {
+    return () => {
+      dispatch(initAllMentor());
+    };
+  }, []);
+
   return (
     <Layout>
-      <div className="app-container">
-        {mentorCardData && !mentorCardLoading && (
+      <div className="app-container home">
+        {allMentor && (
           <InfiniteScroll
-            dataLength={mentorCardData.length}
+            dataLength={allMentor.length}
             next={fetchMoreData}
             hasMore={hasMore}
-            loader={
-              <div
-                className="example"
-                style={{ verticalAlign: "middle", alignContent: "center" }}
-              >
-                로딩중.
-              </div>
-            }
+            loader={<Spin />}
+            className="overflow-none overscroll-y-none overflow-hidden"
           >
             <Typo />
-            <Divider className="dark:bg-slate-400 bg-slate-500 " />
-            <div className="my-[20vh]" />
-            <div className="w-[95vw] grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-3 md:gap-10 p-0 md:p-5 md:w-[80vw]">
-              {mentorCardData.map((mentor) => (
+            <Divider className="divider" />
+            <div className="home-space" />
+            <div className="mentor-cards-container">
+              {allMentor.map((mentor) => (
                 <MentorCard data={mentor} key={mentor.id} />
               ))}
             </div>
