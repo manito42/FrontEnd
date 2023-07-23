aa3bdb0980c62db38ad90eb9b345e95d20a1b42f
9utty <leegh4250@gmail.com>
2023-07-23 23:30:46 +0900

    기능추가: EnrollSlice, enrollApi 추가

--- 42manito/src/RTK/rootReducer.ts
+++ 42manito/src/RTK/rootReducer.ts
@@ -4,6 +4,7 @@ import { GlobalSlice } from "./Slices/Global";
 import { HomeSlice } from "./Slices/Home";
 import { ProfileUpdateSlice } from "./Slices/ProfileUpdate";
 import { CategorySlice } from "./Slices/Category";
+import { EnrollSlice } from "./Slices/Enroll";
 
 export const rootReducer = combineReducers({
   global: GlobalSlice.reducer,
@@ -11,4 +12,5 @@ export const rootReducer = combineReducers({
   home: HomeSlice.reducer,
   currMentor: CurrMentorSlice.reducer,
   category: CategorySlice.reducer,
+  enroll: EnrollSlice.reducer,
 });
--- 42manito/src/RTK/store.ts
+++ 42manito/src/RTK/store.ts
@@ -5,6 +5,7 @@ import { rootReducer } from "./rootReducer";
 import { homeApi } from "./Apis/Home";
 import { categoryApi } from "./Apis/Category";
 import { authApi } from "./Apis/Auth";
+import { enrollApi } from "./Apis/Enroll";
 
 export const store = configureStore({
   reducer: {
@@ -13,13 +14,15 @@ export const store = configureStore({
     [homeApi.reducerPath]: homeApi.reducer,
     [categoryApi.reducerPath]: categoryApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
+    [enrollApi.reducerPath]: enrollApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(userApi.middleware)
       .concat(homeApi.middleware)
       .concat(categoryApi.middleware)
-      .concat(authApi.middleware),
+      .concat(authApi.middleware)
+      .concat(enrollApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
