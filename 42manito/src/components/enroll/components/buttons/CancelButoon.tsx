0369fca3183e2ad53d6046e0a458f91573ad15d0
9utty <leegh4250@gmail.com>
2023-07-23 23:23:31 +0900

    기능추가: cancel 기능 추가

--- 42manito/src/components/enroll/components/buttons/CancelButoon.tsx
+++ 42manito/src/components/enroll/components/buttons/CancelButoon.tsx
@@ -1,27 +1,47 @@
-import { ReservationPostDto } from "@/Types/Reservations/ReservationPost.dto";
-import React from "react";
+import { usePatchReservationCancelMutation } from "@/RTK/Apis/Enroll";
+import { ReservationPatchCancelReqDto } from "@/Types/Reservations/ReservationPatchCancelReq.dto";
+import ConnectModal from "@/components/conect/ConnectModal";
+import React, { useCallback, useState } from "react";
 
 interface CancelButtonProps {
-  data: ReservationPostDto;
+  data: ReservationPatchCancelReqDto;
   isVisible: boolean;
 }
 
 const CancelButton = ({ data, isVisible }: CancelButtonProps) => {
-  if (!isVisible) return null;
+  const [cancel] = usePatchReservationCancelMutation();
+  const [onConnectModal, setOnConnectModal] = useState<boolean>(false);
 
-  const handleCancel = () => {
-    // TODO : 취소 로직
+  const openConnectModal = () => {
+    setOnConnectModal(true);
   };
 
+  const handleYes = useCallback(() => {
+    cancel({ id: data.id });
+    setOnConnectModal(false);
+  }, [data.id]);
+
+  const closeConnectModal = useCallback(() => {
+    setOnConnectModal(false);
+  }, []);
+
+  if (!isVisible) return null;
   return (
     <div className="mt-4">
       <button
         className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
         type="button"
-        onClick={handleCancel}
+        onClick={openConnectModal}
       >
         취소
       </button>
+      {onConnectModal && (
+        <ConnectModal
+          message="취소하시겠습니까?"
+          onClose={closeConnectModal}
+          handleYes={handleYes}
+        />
+      )}
     </div>
   );
 };
