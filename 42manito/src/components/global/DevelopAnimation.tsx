caeec0c41315b5879b6f08bae459662340ecc47c
9utty <leegh4250@gmail.com>
2023-07-24 20:09:42 +0900

    기능수정: size를 선택할 수 있게 변경

--- 42manito/src/components/global/DevelopAnimation.tsx
+++ 42manito/src/components/global/DevelopAnimation.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import Lottie from "react-lottie-player";
 import lottieJson from "../../../public/developer.json";
 
-const DevelopAnimation = () => {
+interface props {
+  size?: string;
+}
+
+const DevelopAnimation = ({ size }: props) => {
   return (
     <Lottie
       loop
       animationData={lottieJson}
       play
-      style={{ width: "35vh", height: "35vh" }}
+      style={{ width: size ? size : "35vh", height: size ? size : "35vh" }}
     />
   );
 };
--- 42manito/src/components/global/HobbyAnimation.tsx
+++ 42manito/src/components/global/HobbyAnimation.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import Lottie from "react-lottie-player";
 import lottieJson from "../../../public/hobby.json";
 
-const HobbyAnimation = () => {
+interface props {
+  size?: string;
+}
+
+const HobbyAnimation = ({ size }: props) => {
   return (
     <Lottie
       loop
       animationData={lottieJson}
       play
-      style={{ width: "35vh", height: "35vh" }}
+      style={{ width: size ? size : "35vh", height: size ? size : "35vh" }}
     />
   );
 };
