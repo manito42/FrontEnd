cf2c7bf5694aea5131274db55e74844973e690c3
9utty <leegh4250@gmail.com>
2023-07-29 18:02:01 +0900

    코드수정: Enroll 컴포넌트가 겹쳐서 보이는 현상 해결

--- 42manito/src/components/Enroll/index.tsx
+++ 42manito/src/components/Enroll/index.tsx
@@ -1,11 +1,21 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import TypeIt from "typeit-react";
 import ProfileTypo from "../Profile/Typo";
 import { RootState, useAppDispatch } from "@/RTK/store";
-import { useGetEnrollQuery } from "@/RTK/Apis/Enroll";
-import { Spin } from "antd";
+import {
+  useGetActiveMenteeEnrollmentMutation,
+  useGetActiveMentorEnrollmentMutation,
+} from "@/RTK/Apis/Enroll";
+import { Divider, Spin } from "antd";
 import { useSelector } from "react-redux";
 import EnrollCard from "./components/Card";
+import InfiniteScroll from "react-infinite-scroll-component";
+import {
+  initReservations,
+  setMenteeReservations,
+  setMentorReservations,
+} from "@/RTK/Slices/Enroll";
 
 // TODO: 엔롤 테스트해봐야함
 
@@ -14,30 +24,84 @@ interface props {
 }
 
 const Enroll = ({ viewProfileTypo }: props) => {
+  const [mentorHasMore, setMentorHasMore] = useState(true);
+  const [menteeHasMore, setMenteeHasMore] = useState(true);
+  const [menteePage, setMenteePage] = useState(0);
+  const [mentorPage, setMentorPage] = useState(0);
+
   const Owner = useSelector(
     (state: RootState) => state.rootReducers.global.uId
   );
-  const [page, setPage] = useState(0);
-  const { data, isLoading, error } = useGetEnrollQuery({
-    id: Owner,
-    take: 12,
-    page: page,
-  });
+
+  const mentorList = useSelector(
+    (state: RootState) => state.rootReducers.enroll.mentorReservation
+  );
+  const menteeList = useSelector(
+    (state: RootState) => state.rootReducers.enroll.menteeReservation
+  );
+  const dispatch = useAppDispatch();
+
+  const [
+    getActiveMentorEnrollment,
+    { data: mentorData, isLoading: getMentorLoading },
+  ] = useGetActiveMentorEnrollmentMutation();
+  const [
+    getActiveMenteeEnrollment,
+    { data: menteeData, isLoading: getMenteeLoading },
+  ] = useGetActiveMenteeEnrollmentMutation();
+
+  const fetchMoreMentorData = () => {
+    getActiveMentorEnrollment({ take: 12, page: mentorPage, id: Owner });
+    setMentorPage(mentorPage + 1);
+  };
+
+  const fetchMoreMenteeData = () => {
+    getActiveMenteeEnrollment({ take: 12, page: menteePage, id: Owner });
+    setMenteePage(menteePage + 1);
+  };
 
   useEffect(() => {
-    if (data) {
-      setPage(page + 1);
+    if (mentorData) {
+      if (mentorData.length % 12 !== 0) {
+        setMentorHasMore(false);
+      } else if (mentorData.length === 0) {
+        setMentorHasMore(false);
+      } else {
+        dispatch(setMentorReservations(mentorData));
+      }
     }
-  }, [data, isLoading, page]);
+  }, [getMentorLoading]);
 
-  if (isLoading) {
-    return <Spin />;
-  }
+  useEffect(() => {
+    if (menteeData) {
+      if (menteeData.length % 12 !== 0) {
+        setMenteeHasMore(false);
+      } else if (menteeData.length === 0) {
+        setMenteeHasMore(false);
+      } else {
+        dispatch(setMenteeReservations(menteeData));
+      }
+    }
+  }, [getMenteeLoading]);
+
+  useEffect(() => {
+    dispatch(initReservations());
+    if (Owner !== 0) {
+      getActiveMentorEnrollment({ take: 12, page: 0, id: Owner });
+      getActiveMenteeEnrollment({ take: 12, page: 0, id: Owner });
+      setMentorPage(1);
+      setMenteePage(1);
+    }
+    return () => {
+      dispatch(initReservations());
+    };
+  }, []);
+
   return (
-    <div className="w-[90vw] md:w-full">
-      <div className="w-full dark:bg-white/40 bg-black/40 h-[1px]" />
-      <div className="grid grid-rows-2 xl:grid-cols-2 gap-y-6 xl:gap-y-0 items-center my-16 xl:max-h-[50vh] md:mt-32 lg:-mb-32">
-        <div className="flex-row justify-center w-full lg:flex items-center">
+    <div className="w-[90vw] md:w-full flex flex-col">
+      <Divider className="dark:bg-slate-400 bg-slate-500" />
+      <div className="flex flex-wrap md:justify-around my-16 w-full">
+        <div className="flex-1 flex flex-row md:flex-col justify-center lg:flex items-center h-[50vh] w-full lg:w-[50%]">
           <div className="text-4xl">
             <TypeIt
               options={{
@@ -58,14 +122,21 @@ const Enroll = ({ viewProfileTypo }: props) => {
               }}
             />
           </div>
-          <div className="overflow-y-auto w-full mx-10 max-h-[50vh]">
-            <InfinityScroll></InfinityScroll>
-            {data?.mentorReservations.map((data) => (
-              <EnrollCard data={data} key={data.id} isMentor={true} />
-            ))}
-          </div>
+          {mentorList && (
+            <InfiniteScroll
+              dataLength={mentorList.length}
+              next={fetchMoreMentorData}
+              hasMore={mentorHasMore}
+              loader={<Spin />}
+              className="overflow-y-auto w-full mx-10"
+            >
+              {mentorList.map((data) => (
+                <EnrollCard data={data} key={data.id} isMentor={true} />
+              ))}
+            </InfiniteScroll>
+          )}
         </div>
-        <div className="flex-row justify-center w-full lg:flex items-center mt-16 lg:mt-0">
+        <div className="flex-1 flex flex-row md:flex-col justify-center lg:flex items-center mt-16 lg:mt-0 h-[50vh] w-full lg:w-[50%]">
           <div className="text-4xl">
             <TypeIt
               options={{
@@ -86,13 +157,22 @@ const Enroll = ({ viewProfileTypo }: props) => {
               }}
             />
           </div>
-          <div className="overflow-y-auto w-full mx-10 max-h-[50vh]">
-            {data?.menteeReservations.map((data) => (
-              <EnrollCard data={data} key={data.id} isMentor={false} />
-            ))}
-          </div>
+          {menteeList && (
+            <InfiniteScroll
+              dataLength={menteeList.length}
+              next={fetchMoreMenteeData}
+              hasMore={menteeHasMore}
+              loader={<Spin />}
+              className="overflow-y-auto w-full mx-10"
+            >
+              {menteeList.map((data) => (
+                <EnrollCard data={data} key={data.id} isMentor={false} />
+              ))}
+            </InfiniteScroll>
+          )}
         </div>
       </div>
+      <Divider className="dark:bg-slate-400 bg-slate-500" />
       <div className="">{!viewProfileTypo && <ProfileTypo />}</div>
     </div>
   );
