7526f0b6b4b2c94a40f79dd3c1d9d2a3124a72ac
9utty <leegh4250@gmail.com>
2023-08-14 18:29:46 +0900

    🌈 수정: 컴포넌트 분리

--- 42manito/src/components/Layout/Header/index.tsx
+++ 42manito/src/components/Layout/Header/index.tsx
@@ -1,14 +1,12 @@
-import React, { memo, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { Divider, Drawer } from "antd";
-import { useAuthSignInMutation, useLoginMutation } from "@/RTK/Apis/Auth";
+import { Drawer } from "antd";
 import { RootState, useAppDispatch } from "@/RTK/store";
-import { signIn, signOut } from "@/RTK/Slices/Global";
-import { useRouter } from "next/router";
+import { signIn } from "@/RTK/Slices/Global";
 import { useSelector } from "react-redux";
-import { SignIn } from "@/utils/SignIn";
-import DarkModeToggleButton from "./DarkModeButton";
-import SearchInput from "../Search/SearchInput";
+import DarkModeToggleButton from "../DarkModeButton";
+import SearchInput from "../../Search/SearchInput";
+import DrawerContent from "./components/DrawerContent";
 
 export default function Header() {
   const [visible, setVisible] = useState(false);
@@ -22,16 +20,6 @@ export default function Header() {
   };
   const onClose = () => {
     setVisible(false);
-  };
-
-  const handleSingOut = async () => {
-    const id = localStorage.getItem("uid");
-    if (id) {
-      await localStorage.removeItem("uid");
-      await localStorage.removeItem("accessToken");
-      dispatch(signOut());
-      location.reload();
-    }
   };
 
   useEffect(() => {
@@ -52,7 +40,7 @@ export default function Header() {
             href="/"
             className="flex title-font font-medium items-center mb-4 md:mb-0"
           >
-            <span className="ml-3 text-4xl hover:scale-105 hover:text-indigo-500">
+            <span className=" text-4xl hover:scale-105 hover:text-indigo-500">
               42Manito
             </span>
           </Link>
@@ -60,7 +48,7 @@ export default function Header() {
             <SearchInput />
           </div>
           <nav className="md:ml-auto flex flex-wrap items-center justify-center mt-5 md:mt-0">
-            <div className="mr-5 btn-drawer" onClick={showDrawer}>
+            <button className="mr-5 btn-drawer" onClick={showDrawer}>
               <svg
                 width="45px"
                 height="45px"
@@ -74,7 +62,7 @@ export default function Header() {
                   d="M5 7C5 6.44772 5.44772 6 6 6H18C18.5523 6 19 6.44772 19 7C19 7.55228 18.5523 8 18 8H6C5.44772 8 5 7.55228 5 7ZM5 12C5 11.4477 5.44772 11 6 11H18C18.5523 11 19 11.4477 19 12C19 12.5523 18.5523 13 18 13H6C5.44772 13 5 12.5523 5 12ZM5 17C5 16.4477 5.44772 16 6 16H18C18.5523 16 19 16.4477 19 17C19 17.5523 18.5523 18 18 18H6C5.44772 18 5 17.5523 5 17Z"
                 />
               </svg>
-            </div>
+            </button>
             <DarkModeToggleButton />
           </nav>
           <Drawer
@@ -84,36 +72,7 @@ export default function Header() {
             onClose={onClose}
             open={visible}
           >
-            <Link href="/About">
-              <p className="text-xl px-3 mt-10 m-5 btn-drawer">About.</p>
-            </Link>
-            {Owner !== 0 && !Number.isNaN(Owner) && (
-              <Link href={`/Profile/${Owner}`}>
-                <p className="text-xl px-3 m-5 btn-drawer">Profile.</p>
-              </Link>
-            )}
-            <Link href="/Categories">
-              <p className="text-xl px-3 m-5 btn-drawer">Category.</p>
-            </Link>
-            {/* <Link href="/Ranking">
-              <p className="text-xl px-3 m-5 btn-drawer">Ranking.</p>
-            </Link> */}
-            {/* {Owner !== 0 && !Number.isNaN(Owner) && (
-              <Link href="/Feedback">
-                <p className="text-xl px-3 m-5 btn-drawer">Feedback.</p>
-              </Link>
-            )} */}
-            <Divider className="dark:bg-slate-400 bg-slate-500 my-16" />
-            {Owner === 0 && (
-              <div id="42AuthSignIn" onClick={() => SignIn()}>
-                <p className="text-xl  px-3 m-5 btn-drawer">Sign In.</p>
-              </div>
-            )}
-            {Owner !== 0 && !Number.isNaN(Owner) && (
-              <div id="SignOut" onClick={handleSingOut}>
-                <p className="text-xl px-3 m-5 btn-drawer">Sign Out.</p>
-              </div>
-            )}
+            <DrawerContent />
           </Drawer>
         </div>
       </header>
