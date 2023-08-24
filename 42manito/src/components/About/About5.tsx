f1648bd2c2c43b31226ce8c2346ba9ff196228d5
9utty <leegh4250@gmail.com>
2023-08-24 17:03:40 +0900

    ✨ feat : comment update

--- 42manito/src/components/About/About5.tsx
+++ 42manito/src/components/About/About5.tsx
@@ -4,7 +4,7 @@ export default function About5() {
   return (
     <div className="about" id="about5">
       <div className="about-show" data-id="about5">
-        <span className="about-title">{`"42마니또와 함께 공부와 취미를 도전하며 도움과 격려를 받아보세요!"`}</span>
+        <span className="about-title">{`"마니또에서 동료들과 함께 도전하며 도움과 격려를 받아보세요!"`}</span>
       </div>
     </div>
   );
