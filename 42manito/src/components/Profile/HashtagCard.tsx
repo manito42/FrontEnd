5f576529c1ef7c4a005e48b134ac060a1fefe479
9utty <leegh4250@gmail.com>
2023-08-29 19:28:40 +0900

    ✅ Update: children 받을 수 있게 수정

--- 42manito/src/components/Profile/HashtagCard.tsx
+++ 42manito/src/components/Profile/HashtagCard.tsx
@@ -1,19 +1,17 @@
-import React from "react";
+import React, { ComponentProps } from "react";
 
 interface props {
   hashtag: string;
+  children?: React.ReactNode;
 }
 
-const HashtagCard = ({ hashtag }: props) => {
-	// TODO: 해시태그 삭제 기능 구현
+const HashtagCard = ({ hashtag, children }: props) => {
   return (
     <div className="flex flex-row mx-3">
       <h6 className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 ">
         {hashtag}
       </h6>
-      <button className="text-slate-600 dark:text-white text-2xl font-bold">
-        X
-      </button>
+      {children && children}
     </div>
   );
 };
