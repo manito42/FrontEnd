80814a64810a9aa0787cb20c07893ebd0a672d6c
9utty <leegh4250@gmail.com>
2023-08-31 20:54:44 +0900

    ✅ request: 프로필 업데이트 수정(카테고리는 일단 around로 수정 1개만 선택)

--- 42manito/src/pages/ProfileUpdate/[userId].tsx
+++ 42manito/src/pages/ProfileUpdate/[userId].tsx
@@ -50,9 +50,25 @@ export default function ProfileUpdate() {
   };
 
   const hashtagPostHandler = () => {
-    console.log(hashTagName);
-    hashtagPost({ name: hashTagName });
-    setHashTagName("");
+    const doesHashtagExist = formData.hashtags.some(
+      (hashtag) => hashtag.name === hashTagName
+    );
+
+    // 정규 표현식으로 영어(a-z, A-Z), 한글(가-힣), 숫자(0-9)를 제외한 모든 문자 찾기
+    const regex = /[^a-zA-Z0-9가-힣]/g;
+
+    if (
+      !doesHashtagExist &&
+      hashTagName.length >= 2 &&
+      !regex.test(hashTagName)
+    ) {
+      // 입력된 해시태그가 위의 조건들을 만족하면 새로운 해시태그 추가
+      hashtagPost({ name: hashTagName });
+      setHashTagName("");
+    } else if (regex.test(hashTagName)) {
+      alert("Only English letters, Korean characters and numbers are allowed.");
+      setHashTagName("");
+    }
   };
 
   const updateButtonHandler = () => {
@@ -78,6 +94,12 @@ export default function ProfileUpdate() {
         id: Number(userId as string),
         profile: form,
       });
+    }
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter" && hashTagName.trim() !== "") {
+      hashtagPostHandler();
     }
   };
 
@@ -124,7 +146,7 @@ export default function ProfileUpdate() {
           </Button>
         </div>
         <div className="mt-32 flex flex-col w-[90%]">
-          <div className="flex flex-row flex-wrap justify-between my-10">
+          <div className="flex flex-row flex-wrap justify-around my-10">
             <div className="flex flex-col items-center">
               <input
                 className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 focus:ring-pink-500 dark:focus:ring-pink-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
@@ -152,40 +174,44 @@ export default function ProfileUpdate() {
           </div>
           <div className="flex w-full flex-col">
             <div className="w-full text-3xl font-bold mb-5">해시태그.</div>
-            <div className="flex flex-row my-3 px-8">
-              {formData &&
-                formData.hashtags.map((hashtag, index) => (
-                  <HashtagCard hashtag={hashtag.name} key={index}>
-                    <button
-                      className="text-slate-600 dark:text-white text-2xl font-bold"
-                      onClick={() =>
-                        dispatch(
-                          ProfileUpdateSlice.actions.deleteOneHashtag(
-                            hashtag.name
+
+            <div className="flex flex-wrap w-full justify-around items-center">
+              <div className="flex flex-row flex-wrap my-3 px-8 w-[35vw]">
+                {formData &&
+                  formData.hashtags.map((hashtag, index) => (
+                    <HashtagCard hashtag={hashtag.name} key={index}>
+                      <button
+                        className="text-slate-600 dark:text-white text-2xl font-bold"
+                        onClick={() =>
+                          dispatch(
+                            ProfileUpdateSlice.actions.deleteOneHashtag(
+                              hashtag.name
+                            )
                           )
-                        )
-                      }
-                    >
-                      X
-                    </button>
-                  </HashtagCard>
-                ))}
-            </div>
-            <div className="flex flex-row w-full justify-between items-center">
-              <TextArea
-                placeholder="Hashtag: ex) #프론트엔드"
-                maxLength={20}
-                value={hashTagName}
-                onChange={(e) => setHashTagName(e.target.value)}
-                className="m-10 h-10 w-full whitespace-pre-wrap"
-              />
-              <Button
-                onClick={() => hashtagPostHandler()}
-                className="flex-shrink-0 h-10 bg-pink-500 hover:bg-pink-600  text-s font-bold text-white py-1 px-2 rounded"
-                type="button"
-              >
-                추가
-              </Button>
+                        }
+                      >
+                        X
+                      </button>
+                    </HashtagCard>
+                  ))}
+              </div>
+              <div>
+                <Input
+                  placeholder="Hashtag: ex) #프론트엔드"
+                  maxLength={20}
+                  value={hashTagName}
+                  onChange={(e) => setHashTagName(e.target.value)}
+                  onKeyPress={handleKeyPress}
+                  className="m-10 h-10 w-[30vw] whitespace-pre-wrap"
+                />
+                <Button
+                  onClick={() => hashtagPostHandler()}
+                  className="flex-shrink-0 h-10 bg-pink-500 hover:bg-pink-600  text-s font-bold text-white py-1 px-2 rounded"
+                  type="button"
+                >
+                  추가
+                </Button>
+              </div>
             </div>
           </div>
           <div className="w-full my-8">
