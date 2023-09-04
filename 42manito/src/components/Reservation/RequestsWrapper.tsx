8803ccfe7d84c24db44c385dd7ffedf84d904a94
falconlee236 <falconlee236@cau.ac.kr>
2023-09-04 23:32:15 +0900

    Update Component/Reservation/RequestWrapper.tsx: Update with overflow-x-scroll but do not activate

--- 42manito/src/components/Reservation/RequestsWrapper.tsx
+++ 42manito/src/components/Reservation/RequestsWrapper.tsx
@@ -1,4 +1,4 @@
-import {useFetchHome} from "@/hooks/Home/FetchHome";
+import { useFetchHome } from "@/hooks/Home/FetchHome";
 import Image from "next/image";
 import React from "react";
 
@@ -6,22 +6,23 @@ const RequestsWrapper = () => {
     const { allMentor, fetchMoreData, hasMore } = useFetchHome();
 
     return (
-        <div className="flex flex-row overflow-x-auto">
-            {allMentor.map((data) => (
-                // eslint-disable-next-line react/jsx-key
-                <div>
-                    <Image
-                        className="mentor-image-container"
-                        src={data.user.profileImage}
-                        alt="cover image"
-                        width={100}
-                        height={100}
-                        quality={80}
-                    />
-                </div>
-            ))}
+        <div className="overflow-x-scroll w-[90vw] h-[50vh] bg-[#475569]">
+            <div className="flex flex-row flex-nowrap">
+                {allMentor.map((data, index) => (
+                    <div key={index} className="h-[40vh] w-[30vh] m-2 rounded-xl bg-[#374151]">
+                        <Image
+                            className="rounded-full"
+                            src={data.user.profileImage}
+                            alt="cover image"
+                            width={100}
+                            height={100}
+                            quality={80}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default RequestsWrapper;
