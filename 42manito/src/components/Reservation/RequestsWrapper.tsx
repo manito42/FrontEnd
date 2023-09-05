7243595700c0d5aff9059d5bda0a7f0693b7c769
falconlee236 <falconlee236@cau.ac.kr>
2023-09-05 14:42:00 +0900

    Update RequestWrapper.tsx: finally add horizontal scroll axis-x with whitespace-nowrap and inline-block

--- 42manito/src/components/Reservation/RequestsWrapper.tsx
+++ 42manito/src/components/Reservation/RequestsWrapper.tsx
@@ -4,23 +4,26 @@ import React from "react";
 
 const RequestsWrapper = () => {
     const { allMentor, fetchMoreData, hasMore } = useFetchHome();
+  let testArr: number[];
+  // @ts-ignore
+  testArr = [...Array(100).keys()];
 
     return (
-        <div className="overflow-x-scroll w-[90vw] h-[50vh] bg-[#475569]">
-            <div className="flex flex-row flex-nowrap">
-                {allMentor.map((data, index) => (
-                    <div key={index} className="h-[40vh] w-[30vh] m-2 rounded-xl bg-[#374151]">
-                        <Image
-                            className="rounded-full"
-                            src={data.user.profileImage}
-                            alt="cover image"
-                            width={100}
-                            height={100}
-                            quality={80}
-                        />
-                    </div>
-                ))}
-            </div>
+        <div className="overflow-x-scroll overflow-y-hidden whitespace-nowrap
+                        w-[80vw] h-[50vh] bg-[#475569] flex-nowrap flex-row
+                        justify-center items-center ">
+          {allMentor.map((data, index) => (
+              <div key={index} className="h-[40vh] w-[10vw] m-2 mt-10 rounded-xl bg-[#374151] inline-block">
+                <Image
+                    className="rounded-full"
+                    src={data.user.profileImage}
+                    alt="cover image"
+                    width={100}
+                    height={100}
+                    quality={80}
+                />
+              </div>
+          ))}
         </div>
     );
 };
