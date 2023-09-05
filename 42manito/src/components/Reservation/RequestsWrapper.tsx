f95978c6b2c5275bd9f1ed207b68347580946e51
falconlee236 <falconlee236@cau.ac.kr>
2023-09-05 17:02:09 +0900

    Update RequestWrapper.tsx: move picture and nickname

--- 42manito/src/components/Reservation/RequestsWrapper.tsx
+++ 42manito/src/components/Reservation/RequestsWrapper.tsx
@@ -13,15 +13,25 @@ const RequestsWrapper = () => {
                         w-[80vw] h-[50vh] bg-[#475569] flex-nowrap flex-row
                         justify-center items-center ">
           {allMentor.map((data, index) => (
-              <div key={index} className="h-[40vh] w-[10vw] m-2 mt-10 rounded-xl bg-[#374151] inline-block">
-                <Image
-                    className="rounded-full"
-                    src={data.user.profileImage}
-                    alt="cover image"
-                    width={100}
-                    height={100}
-                    quality={80}
-                />
+              <div key={index} className="h-[35vh] lg:w-[15vw] md:w-[25vw] w-[60%] mt-[3.5rem]
+                                          mx-10 rounded-xl bg-[#374151] inline-block
+                                          shadow-2xl">
+                <div className="flex justify-center items-center">
+                  <div className="relative z-[100] flex justify-center items-center">
+                    <Image
+                        className="shadow-xl rounded-full align-middle border-none m-[4rem] md:mt-20 lg:mx-6 lg:mt-[4rem] object-cover w-[13vh] h-[13vh] md:w-[8vw] md:h-[8vw]"
+                        src={data.user.profileImage}
+                        alt="..."
+                        width={100}
+                        height={100}
+                    />
+                  </div>
+                </div>
+                  <div className="w-full relative -mt-10 lg:-mt-10 flex justify-center">
+                    <div className="w-[50%] truncate text-center font-bold">
+                      {data.user.nickname}
+                    </div>
+                  </div>
               </div>
           ))}
         </div>
