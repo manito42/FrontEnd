f5b339b8586ec256b99c1f46ae057df42eb9eab8
Photogrammer <khsj15@khu.ac.kr>
2023-09-08 06:48:15 +0900

    :lipstick: [Feat]: 로그인 시 로딩창 (component)

---
+++ 42manito/src/components/Global/Loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <>
+      <div className="backdrop-blur-sm w-screen h-full flex justify-center items-center z-[800] fixed mexican-wave">
+        {""}
+      </div>
+    </>
+  );
+}
