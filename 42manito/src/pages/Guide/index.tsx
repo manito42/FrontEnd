3d609c3d0c1a67a78a3df3768a21c42eb84df634
Photogrammer <khsj15@khu.ac.kr>
2023-09-08 19:22:42 +0900

    :lipstick: [UI]: Banner
    - Banner UI 변경
    - left, right 를 구분하고, 모바일에서도 자연스럽게 표현될 수 있도록 함
    - 텍스트 클릭 시 원하는 페이지로 이동할 수 있도록 함
    - 텍스트, 배경 컬러를 지정할 수 있도록 함.

---
+++ 42manito/src/pages/Guide/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "@/components/Layout/Layout";
+const About = () => {
+  return (
+    <Layout>
+      <div className={"about-container"}></div>
+    </Layout>
+  );
+};
+
+export default About;
