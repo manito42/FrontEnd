66bd7b72b8f9a30c6e755af8919c31a0561aeab0
Photogrammer <khsj15@khu.ac.kr>
2023-09-16 17:19:24 +0900

    [update]: Mentoring page
    [update]: 로그인이 안됐을 때 홈으로 리디렉션
    [update]: API 업데이트 및 컴포넌트 변경 반영

--- 42manito/src/pages/Mentorings/index.tsx
+++ 42manito/src/pages/Mentorings/index.tsx
@@ -1,17 +1,14 @@
 import Layout from "@/components/Layout/Layout";
 import ReservationLists from "@/components/Reservation/ReservationLists";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import TopBanner from "@/components/Banners/TopBanner";
-import { UserReservationResDto } from "@/Types/UserReservation/UserReservationResDto";
-import {
-  useGetActiveReservationQuery,
-  useGetRequestReservationsQuery,
-} from "@/RTK/Apis/Reservation";
 import { useSelector } from "react-redux";
 import { RootState } from "@/RTK/store";
-import { ReservationDefaultDto } from "@/Types/Reservations/ReservationDefault.dto";
-import { ReservationRole } from "@/components/Reservation/getReservationStatus";
 import ReservationModal from "@/components/Reservation/modal/ReservationModal";
+import { ReservationRole } from "@/Types/Reservations/ReservationRole";
+import { ReservationStatus } from "@/Types/Reservations/ReservationStatus";
+import { useRouter } from "next/router";
+import Loading from "@/components/Global/Loading";
 
 const Mentoring = () => {
   const banner = [
@@ -27,146 +24,123 @@ const Mentoring = () => {
         "https://kor.pngtree.com/freepng/meb-map-guide_4462396.html' 의 PNG 이미지 kor.pngtree.com",
     },
   ];
+  const router = useRouter();
   const uid = useSelector((state: RootState) => state.rootReducers.global.uId);
   const [requestReservationPage, setRequestReservationPage] = useState(0);
   const [activeReservationPage, setActiveReservationPage] = useState(0);
   const [inactiveReservationPage, setInactiveReservationPage] = useState(0);
-  const [role, setRole] = useState<ReservationRole | "all">("all");
+  const [role, setRole] = useState<ReservationRole>(ReservationRole.ALL);
   const isModalOpen = useSelector(
     (state: RootState) => state.rootReducers.reservation.isModalOpen,
   );
   const selectedReservation = useSelector(
     (state: RootState) => state.rootReducers.reservation.selectedReservation,
   );
-  const { data: requestResponse }: UserReservationResDto =
-    useGetRequestReservationsQuery({ id: uid });
-  const { data: activeResponse }: UserReservationResDto =
-    useGetActiveReservationQuery({
-      id: uid,
-      take: 10,
-      page: activeReservationPage,
-      as_mentor: role === ReservationRole.Mentor || role === "all",
-      as_mentee: role === ReservationRole.Mentee || role === "all",
-      active: true,
-    });
-  const { data: inactiveResponse }: UserReservationResDto =
-    useGetActiveReservationQuery({
-      id: uid,
-      take: 10,
-      page: inactiveReservationPage,
-      as_mentor: role === ReservationRole.Mentor || role === "all",
-      as_mentee: role === ReservationRole.Mentee || role === "all",
-      active: false,
-    });
-  const [requestReservations, setRequestReservations] = useState<
-    ReservationDefaultDto[]
-  >([]);
-  const [activeReservations, setActiveReservations] = useState<
-    ReservationDefaultDto[]
-  >([]);
-  const [inactiveReservations, setInactiveReservations] = useState<
-    ReservationDefaultDto[]
-  >([]);
-
-  useEffect(() => {
-    if (requestResponse) {
-      setRequestReservations([
-        ...requestResponse.mentorReservations,
-        ...requestResponse.menteeReservations,
-      ]);
-    }
-  }, [requestResponse]);
-  useEffect(() => {
-    if (activeResponse) {
-      setActiveReservations([
-        ...(activeResponse?.mentorReservations || []),
-        ...(activeResponse?.menteeReservations || []),
-      ]);
-    }
-  }, [activeResponse]);
-  useEffect(() => {
-    if (inactiveResponse) {
-      setInactiveReservations([
-        ...(inactiveResponse?.mentorReservations || []),
-        ...(inactiveResponse?.menteeReservations || []),
-      ]);
-    }
-  }, [inactiveResponse]);
-
+  const requestQuery = {
+    take: 100,
+    page: requestReservationPage,
+    role: role,
+    status: [ReservationStatus.REQUEST, ReservationStatus.ACCEPT],
+  };
+  const activeQuery = {
+    take: 10,
+    page: activeReservationPage,
+    role: role,
+    status: [
+      ReservationStatus.MENTEE_FEEDBACK,
+      ReservationStatus.MENTEE_CHECKED,
+    ],
+  };
+  const inActiveQuery = {
+    take: 10,
+    page: inactiveReservationPage,
+    role: role,
+    status: [ReservationStatus.DONE, ReservationStatus.CANCEL],
+  };
   const handleRoleSelect = (id: string) => {
     if (id === "mentor") {
-      setRole(ReservationRole.Mentor);
+      setRole(ReservationRole.MENTOR);
     } else if (id === "mentee") {
-      setRole(ReservationRole.Mentee);
+      setRole(ReservationRole.MENTEE);
     } else {
-      setRole("all");
+      setRole(ReservationRole.ALL);
     }
   };
+
+  if (typeof window !== "undefined" && uid === 0) {
+    alert("로그인이 필요합니다");
+    router.push("/");
+  }
 
   return (
     <>
       <Layout>
         <div className="app-container">
           <TopBanner banner={banner} />
-          <div className="mentoring-wrapper">
-            <div className="mentoring-select-role">
-              <div
-                className={`mentoring-select-role-header ${
-                  role === "all" ? "active-role" : "inactive-role"
-                }`}
-                id="all"
-                onClick={(e) => {
-                  handleRoleSelect(e.currentTarget.id);
-                }}
-              >
-                전체
+          {uid === null && <Loading />}
+          {!!uid && (
+            <div className="mentoring-wrapper">
+              <div className="mentoring-select-role">
+                <div
+                  className={`mentoring-select-role-header ${
+                    role === ReservationRole.ALL
+                      ? "active-role"
+                      : "inactive-role"
+                  }`}
+                  id="all"
+                  onClick={(e) => {
+                    handleRoleSelect(e.currentTarget.id);
+                  }}
+                >
+                  전체
+                </div>
+                <div
+                  className={`mentoring-select-role-header ${
+                    role === ReservationRole.MENTOR
+                      ? "active-role"
+                      : "inactive-role"
+                  }`}
+                  id="mentor"
+                  onClick={(e) => {
+                    handleRoleSelect(e.currentTarget.id);
+                  }}
+                >
+                  멘토
+                </div>
+                <div
+                  className={`mentoring-select-role-header ${
+                    role === ReservationRole.MENTEE
+                      ? "active-role"
+                      : "inactive-role"
+                  }`}
+                  id="mentee"
+                  onClick={(e) => {
+                    handleRoleSelect(e.currentTarget.id);
+                  }}
+                >
+                  멘티
+                </div>
               </div>
-              <div
-                className={`mentoring-select-role-header ${
-                  role === ReservationRole.Mentor
-                    ? "active-role"
-                    : "inactive-role"
-                }`}
-                id="mentor"
-                onClick={(e) => {
-                  handleRoleSelect(e.currentTarget.id);
-                }}
-              >
-                멘토
-              </div>
-              <div
-                className={`mentoring-select-role-header ${
-                  role === ReservationRole.Mentee
-                    ? "active-role"
-                    : "inactive-role"
-                }`}
-                id="mentee"
-                onClick={(e) => {
-                  handleRoleSelect(e.currentTarget.id);
-                }}
-              >
-                멘티
-              </div>
+              <div className="mentoring-text-header">대기 중인 멘토링</div>
+              <ReservationLists
+                query={requestQuery}
+                name={"request"}
+                emptyMsg={"대기중인 멘토링이 없습니다 🥲"}
+              />
+              <div className="mentoring-text-header">진행 중인 멘토링</div>
+              <ReservationLists
+                query={activeQuery}
+                name={"active"}
+                emptyMsg={"진행중인 멘토링이 없습니다 🥲"}
+              />
+              <div className="mentoring-text-header">완료된 멘토링</div>
+              <ReservationLists
+                query={inActiveQuery}
+                name={"inactive"}
+                emptyMsg={"완료된 멘토링이 없습니다 🥲"}
+              />
             </div>
-            <div className="mentoring-text-header">대기 중인 멘토링</div>
-            <ReservationLists
-              reservations={requestReservations}
-              name={"request"}
-              emptyMsg={"대기중인 멘토링이 없습니다 🥲"}
-            />
-            <div className="mentoring-text-header">진행 중인 멘토링</div>
-            <ReservationLists
-              reservations={activeReservations}
-              name={"active"}
-              emptyMsg={"진행중인 멘토링이 없습니다 🥲"}
-            />
-            <div className="mentoring-text-header">완료된 멘토링</div>
-            <ReservationLists
-              reservations={inactiveReservations}
-              name={"inactive"}
-              emptyMsg={"완료된 멘토링이 없습니다 🥲"}
-            />
-          </div>
+          )}
         </div>
         {isModalOpen && selectedReservation !== null && <ReservationModal />}
       </Layout>
