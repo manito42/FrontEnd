8497104d17815054a147715b4903f7348377ff88
Photogrammer <khsj15@khu.ac.kr>
2023-09-18 16:18:28 +0900

    [fix]: 프로필 내의 멘토링 횟수 정보 제거

--- 42manito/src/components/Profile/Info.tsx
+++ 42manito/src/components/Profile/Info.tsx
@@ -10,18 +10,6 @@ export default function ProfileInfo({ nickname, count }: props) {
     <div className="profile-info-wrapper">
       <div className="" id="Count">
         <div className="profile-info-container">
-          {count !== undefined ? (
-            <div className="p-3 text-center">
-              <span className="text-3xl font-bold block uppercase tracking-wide text-bg_color-800 dark:text-bg_color-200">
-                {count}
-              </span>
-              <span className="text-xl text-bg_color-600 dark:text-bg_color-400">
-                Total
-              </span>
-            </div>
-          ) : (
-            <></>
-          )}
           <div className="text-4xl font-bold flex justify-center items-center flex-col">
             {nickname}
           </div>
